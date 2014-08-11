d7cbac861d0db9c98fd9bb7db628a602433e2e6a
Jo De Boeck <deboeck.jo@gmail.com>
2014-08-12 00:08:15 +0300

    Allow user to choose his download location

--- addon.py
+++ addon.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import xbmc
+import xbmcaddon
 import xbmcgui
 import sys
 import os
@@ -8,6 +9,10 @@ import xbmcvfs
 import json
 
 pluginhandle=int(sys.argv[1])
+addon = xbmcaddon.Addon("plugin.video.makeoffline")
+storelocation = "storelocation"
+basepath = addon.getSetting(storelocation)
+
 lastContentType = xbmc.getInfoLabel('Container.FolderPath')
 path = xbmc.getInfoLabel('ListItem.FileNameAndPath')
 title = xbmc.getInfoLabel('ListItem.Title')
@@ -16,15 +21,25 @@ def makeRequest(call, **kwargs):
     request = {"jsonrpc": "2.0", "method": call, "id": 1, "params": kwargs}
     return json.loads(xbmc.executeJSONRPC(json.dumps(request)))
 
-sources = makeRequest('Files.GetSources', media='video')
-basepath = None
-for source in sources['result']['sources']:
-    if os.path.exists(source['file']):
-            basepath = source['file']
 if not basepath:
+    options = list()
+    sources = makeRequest('Files.GetSources', media='video')
+    for source in sources['result']['sources']:
+        if os.path.exists(source['file']):
+            options.append(source['file'])
+    if options:
+        options.append("Enter Manually")
+        dialog = xbmcgui.Dialog()
+        result = dialog.select("Choose Location", options)
+        if result < len(options) - 1:
+            basepath = options[result]
+    if not basepath:
+        dialog = xbmcgui.Dialog()
+        basepath = dialog.browseSingle(3, "Select download folder", "videos")
     dialog = xbmcgui.Dialog()
-    dialog.notification("Failed to download", "No local source found")
-    sys.exit(0)
+    answer = dialog.yesno("Always use this path?", basepath)
+    if answer:
+        addon.setSetting(storelocation, basepath)
 
 if os.path.exists(path):
     dialog = xbmcgui.Dialog()
