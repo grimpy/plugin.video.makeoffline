eebe3bb78e2a004768784527e9a98b1231f120c0
Jo De Boeck <deboeck.jo@gmail.com>
2014-08-11 23:23:37 +0300

    Get basefolder from sources + do scan after downloading file

--- addon.py
+++ addon.py
@@ -5,13 +5,27 @@ import xbmcgui
 import sys
 import os
 import xbmcvfs
+import json
 
 pluginhandle=int(sys.argv[1])
 lastContentType = xbmc.getInfoLabel('Container.FolderPath')
 path = xbmc.getInfoLabel('ListItem.FileNameAndPath')
 title = xbmc.getInfoLabel('ListItem.Title')
 
-basepath = '/home/Jo/Videos'
+def makeRequest(call, **kwargs):
+    request = {"jsonrpc": "2.0", "method": call, "id": 1, "params": kwargs}
+    return json.loads(xbmc.executeJSONRPC(json.dumps(request)))
+
+sources = makeRequest('Files.GetSources', media='video')
+basepath = None
+for source in sources['result']['sources']:
+    if os.path.exists(source['file']):
+            basepath = source['file']
+if not basepath:
+    dialog = xbmcgui.Dialog()
+    dialog.notification("Failed to download", "No local source found")
+    sys.exit(0)
+
 if os.path.exists(path):
     dialog = xbmcgui.Dialog()
     dialog.notification("Local file", "Nothing to do")
@@ -55,3 +69,5 @@ with open(newpath, "w") as fd:
 
 if progress.iscanceled():
     os.remove(newpath)
+else:
+    makeRequest('VideoLibrary.Scan')
